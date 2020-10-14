50f5e70377e0de06ad2aca52ade346cf07a96c74
Kycer <978262587@qq.com>
2020-10-14 19:37:51 +0800

    change config

--- config.h
+++ config.h
@@ -1,28 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Han Sans CN:size=16" };
 static const char dmenufont[]       = "Source Han Sans CN:size=16";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#A162F7";
+static char selbgcolor[]            = "#005577";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 static const unsigned int alphas[][3]      = {
@@ -88,7 +89,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 // scratchpad
@@ -107,8 +108,6 @@ static const char *voldowncmd[]  = { "/home/kycer/.dotfiles/scripts/vol.sh", "-d
 static const char *volmutecmd[]  = { "/home/kycer/.dotfiles/scripts/vol.sh", "-m", NULL };
 // trayer
 static const char *trayercmd[]  = { "/home/kycer/.dotfiles/scripts/traye-tg.sh", NULL };
-// lock
-static const char *lockcmd[]  = { "betterlockscreen", "-l", "dim", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -123,7 +122,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_d, 		 spawn,  				 {.v = voldowncmd } },
 	{ MODKEY|ShiftMask,             XK_m, 		 spawn,  				 {.v = volmutecmd } },
 	{ MODKEY|ShiftMask,             XK_t, 		 spawn,  				 {.v = trayercmd } },
-	{ MODKEY|ShiftMask,             XK_l, 		 spawn,  				 {.v = lockcmd } },
+	{ MODKEY|ShiftMask,             XK_l, 		 spawn,  				 SHCMD("betterlockscreen -l dim") },
+	{ MODKEY|ShiftMask,             XK_s, 		 spawn,  				 SHCMD("flameshot gui") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
